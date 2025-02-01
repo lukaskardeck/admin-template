1cf3bf41a5c954e6a8a6675d25476fcb4b4a0a25
lukaskardeck <92531036+lukaskardeck@users.noreply.github.com>
2025-02-01 20:28:20 -0300

    feat: interface do botao de logout

--- src/components/template/menu-item/menuItem.tsx
+++ src/components/template/menu-item/menuItem.tsx
@@ -3,25 +3,37 @@ import { useRouter } from "next/router"
 
 interface MenuItemProps {
     texto: string
-    url: string
     icone: any
+    url?: string
+    className?: string
+    onClick?: (evento: any) => void
 }
 
 export default function MenuItem(props: MenuItemProps) {
     const router = useRouter()
     const paginaSelecionada = router.pathname == props.url ? "bg-gray-100" : ""
 
-    return (
-        <li className={`hover:bg-gray-100 px-1 ${paginaSelecionada}`}>
-            <Link 
-                href={props.url} 
-                className="flex flex-col justify-center items-center h-20 w-full"
-            >
+    function renderizarItem() {
+        return (
+            <div className={`flex flex-col justify-center items-center h-20 w-full}`}>
                 {props.icone}
                 <span
-                    className="text-sm font-light text-gray-600"
+                    className="text-sm font-light"
                 >{props.texto}</span>
-            </Link>
+            </div>
+        )
+    }
+
+    return (
+        <li className={`
+            hover:bg-gray-100 px-1 cursor-pointer text-gray-600 
+            ${paginaSelecionada} ${props.className}
+        `} onClick={props.onClick}>
+            {props.url ? (
+                <Link href={props.url}>
+                    {renderizarItem()}
+                </Link>
+            ) : renderizarItem()}
         </li>
     )
 }
--- src/components/template/menu-lateral/MenuLateral.tsx
+++ src/components/template/menu-lateral/MenuLateral.tsx
@@ -1,14 +1,29 @@
 
-import { IconeAjuste, IconeCasa, IconeSino } from "@/components/icons";
+import { IconeAjuste, IconeCasa, IconeSair, IconeSino } from "@/components/icons";
 import MenuItem from "../menu-item/menuItem";
 
 export default function MenuLateral() {
     return (
-        <aside>
+        <aside className="flex flex-col">
+            <div className={`
+                flex items-center justify-center h-20 w-full 
+                bg-gradient-to-r from-emerald-500 to-lime-600
+                text-white   
+            `}>
+                LOGO
+            </div>
+            <ul className=" flex-1">
+                <MenuItem url="/" texto="Ínicio" icone={IconeCasa} />
+                <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjuste} />
+                <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
+            </ul>
             <ul>
-                <MenuItem url="/" texto="Ínicio" icone={IconeCasa}/>
-                <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjuste}/>
-                <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino}/>
+                <MenuItem 
+                    texto="Sair" 
+                    icone={IconeSair} 
+                    onClick={()=>console.log("LOGOUT")}
+                    className="text-red-600 hover:bg-red-400 hover:text-white"
+                />
             </ul>
         </aside>
     )
