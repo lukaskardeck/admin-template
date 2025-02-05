62048e8fa776217fbb8e0786c9e08c1445d14c87
lukaskardeck <92531036+lukaskardeck@users.noreply.github.com>
2025-02-05 08:56:49 -0300

    feat: mantendo o tema da página após o refresh

--- src/data/context/AppContext.tsx
+++ src/data/context/AppContext.tsx
@@ -1,20 +1,27 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
-export type Tema = 'dark' | ''
+// export type Tema = 'dark' | ''
 
 interface AppContextProps {
-    tema?: Tema
+    tema?: string
     alternarTema?: () => void
 }
 
 const AppContext = createContext<AppContextProps>({})
 
 function AppProvider(props) {
-    const [tema, setTema] = useState<Tema>('')
+    const [tema, setTema] = useState("")
 
     function alternarTema() {
-        setTema(tema === "" ? "dark": "")
+        const novoTema = tema === "" ? "dark": ""
+        setTema(novoTema)
+        localStorage.setItem("tema", novoTema)
     }
+
+    useEffect(() => {
+        const temaSalvo = localStorage.getItem("tema")
+        setTema(temaSalvo ?? "")
+    }, [])
 
     return (
         <AppContext.Provider value={{
